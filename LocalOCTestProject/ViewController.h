d8ddf93aff4dd1c4dc5e5358603e9763a986f188
liuchao <13499668830@qq.com>
2016-07-05 10:29:03 +0800

    初始化的项目文件提交到本地的git仓库中

---
+++ LocalOCTestProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LocalOCTestProject
+//
+//  Created by 刘超 on 16/7/5.
+//  Copyright © 2016年 刘超. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LocalOCTestProject/ViewController.m
@@ -0,0 +1,31 @@
+//
+//  ViewController.m
+//  LocalOCTestProject
+//
+//  Created by 刘超 on 16/7/5.
+//  Copyright © 2016年 刘超. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.view.backgroundColor=[UIColor brownColor];
+    
+    
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
